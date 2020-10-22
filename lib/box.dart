1f55191acb0d419108e150d406798001f44f15cd
Neftali Papelleras <iam@neftalipapelleras.com>
2020-10-22 10:13:16 +0800

    Add jumping ability

--- lib/box.dart
+++ lib/box.dart
@@ -2,10 +2,33 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 class Box {
-  double _y = 125;
+  double y = 0;
+  double _dy = 0;
+  double _jumpForce = 15.0;
+  double height = 25;
+  bool _canJump = true;
+
+  void updatePosition() {
+    y += _dy;
+
+    if (height + y < 150) {
+      _dy += 1.0;
+      _canJump = false;
+    } else {
+      _canJump = true;
+      _dy = 0;
+      y = 150 - height;
+    }
+  }
+
+  void jump() {
+    if (_canJump) {
+      _dy -= _jumpForce;
+    }
+  }
 
   Widget get getBox => Transform.translate(
-        offset: Offset(10, _y),
+        offset: Offset(10, y),
         child: Container(
           width: 25,
           height: 25,
--- lib/engine.dart
+++ lib/engine.dart
@@ -6,21 +6,54 @@ class GameEngine extends StatefulWidget {
   _GameEngineState createState() => _GameEngineState();
 }
 
-class _GameEngineState extends State<GameEngine> {
+class _GameEngineState extends State<GameEngine>
+    with SingleTickerProviderStateMixin {
   final b = Box();
+  AnimationController _animationController;
+
+  @override
+  void initState() {
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+    _animationController.addListener(_updateBoxPosition);
+    _animationController.repeat();
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  void _updateBoxPosition() {
+    if (b != null) {
+      setState(() {
+        b.updatePosition();
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          width: double.infinity,
-          height: 150,
-          color: Colors.grey,
-          child: Stack(
-            children: <Widget>[
-              b.getBox,
-            ],
+      body: GestureDetector(
+        onTap: () {
+          b.jump();
+        },
+        child: Center(
+          child: Container(
+            width: double.infinity,
+            height: 150,
+            color: Colors.grey,
+            child: Stack(
+              children: <Widget>[
+                b.getBox,
+              ],
+            ),
           ),
         ),
       ),
